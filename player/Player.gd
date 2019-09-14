1fe2d951c0929cb825c7e10b661faed9cde3b1b9
Josh Maxwell <cctechwiz@gmail.com>
2019-09-14 07:02:13 -0600

    added demo player animation for walking and idle

--- player/Player.gd
+++ player/Player.gd
@@ -1,11 +1,19 @@
 extends KinematicBody2D
 
 export (int) var speed := 200
+var input_direction := Vector2.ZERO
+var sprite_direction := "down"
 
 
 func _physics_process(delta: float):
-	var input_direction: Vector2 = get_input_direction()
-	move(input_direction)
+	get_input_direction()
+	move()
+	update_sprite()
+
+	if input_direction != Vector2.ZERO:
+		update_animation("walk")
+	else:
+		update_animation("idle")
 
 
 func get_input_direction():
@@ -14,18 +22,34 @@ func get_input_direction():
 	var up: bool = Input.is_action_pressed("ui_up")
 	var down: bool = Input.is_action_pressed("ui_down")
 
-	var input_direction := Vector2(
+	input_direction = Vector2(
 		-int(left) + int(right),
 		-int(up) + int(down)
 	)
 
 	# Prevent diagonal movement
-	if input_direction.x != 0 and input_direction.y != 0:
-		input_direction = Vector2.ZERO
-
-	return input_direction
+	#if input_direction.x != 0 and input_direction.y != 0:
+	#	input_direction = Vector2.ZERO
 
 
-func move(input_direction: Vector2):
+func move():
 	var velocity: Vector2 = (input_direction.normalized() * speed)
-	move_and_slide(velocity, Vector2.ZERO)
+	return move_and_slide(velocity, Vector2.ZERO)
+
+
+func update_sprite():
+	match input_direction:
+		Vector2(-1, 0):
+			sprite_direction = "left"
+		Vector2(1, 0):
+			sprite_direction = "right"
+		Vector2(0, -1):
+			sprite_direction = "up"
+		Vector2(0, 1):
+			sprite_direction = "down"
+
+
+func update_animation(animation):
+	var new_animation = animation + "_" + sprite_direction
+	if $Animation.current_animation != new_animation:
+		$Animation.play(new_animation)
