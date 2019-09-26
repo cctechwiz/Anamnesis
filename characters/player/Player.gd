46c4ada5ce62f1970c19cc63ab306d30c9cddba3
Josh Maxwell <cctechwiz@gmail.com>
2019-09-26 05:16:51 -0600

    Added 8 direction movement back in.

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -1,32 +1,36 @@
 extends KinematicBody2D
+class_name Player
+
+export(int) var speed: = 400
 
 onready var Grid = get_parent()
 
-const UP := Vector2(0, -1)
-const RIGHT_UP := Vector2(1, -1)
-const RIGHT := Vector2(1, 0)
-const RIGHT_DOWN := Vector2(1, 1)
-const DOWN := Vector2(0, 1)
-const LEFT_DOWN := Vector2(-1, 1)
-const LEFT := Vector2(-1, 0)
-const LEFT_UP := Vector2(-1, -1)
+const UP: = Vector2(0, -1)
+const RIGHT_UP: = Vector2(1, -1)
+const RIGHT: = Vector2(1, 0)
+const RIGHT_DOWN: = Vector2(1, 1)
+const DOWN: = Vector2(0, 1)
+const LEFT_DOWN: = Vector2(-1, 1)
+const LEFT: = Vector2(-1, 0)
+const LEFT_UP: = Vector2(-1, -1)
 
 
-func _ready():
+func _ready() -> void:
 	# The default look direction is RIGHT
 	update_look_direction(DOWN)
 
 
-func _process(delta: float):
-	var input_direction = get_input_direction()
+func _process(delta: float) -> void:
+	var input_direction: Vector2 = get_input_direction()
 	if not input_direction:
 		return
 
 	#print_direction_info(input_direction)
 	update_look_direction(input_direction)
+	move_and_slide(input_direction.normalized() * speed)
 
 
-func get_input_direction():
+func get_input_direction() -> Vector2:
 	var left: bool = Input.is_action_pressed("ui_left")
 	var right: bool = Input.is_action_pressed("ui_right")
 	var up: bool = Input.is_action_pressed("ui_up")
@@ -35,14 +39,14 @@ func get_input_direction():
 	return Vector2(int(right) - int(left), int(down) - int(up))
 
 
-func update_look_direction(direction):
+func update_look_direction(direction: Vector2) -> void:
 	# TODO: Change sprite based on look direction
 	# TODO: Update rayCast2D direction
 	$Pivot/Sprite.rotation = direction.angle()
 	$CollisionShape2D.rotation = direction.angle()
 
 
-func print_direction_info(direction):
+func print_direction_info(direction: Vector2) -> void:
 	match(direction):
 		UP:
 			print("UP")
