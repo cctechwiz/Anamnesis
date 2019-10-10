a783bb863e8ab1837ee11da9466ff53ba925e987
Josh Maxwell <cctechwiz@gmail.com>
2019-10-10 06:14:30 -0600

    Removed Player from Grid, playing with animation speed to move character around a little faster
    0.15 seems to be the sweet spot. However, by disabling processing while moving I can still press the keys just too fast so I miss a movement.
    I could play with having an input queue, so every key press gets queued and handled in order no matter what. This would be interesting for holding the keys down as that has fairly rapid repetition.
    
    I'm not convinced I need a "grid" to track where things are. I believe using raycast or 2D collisions will work for this game.

--- game/src/Characters/Player/Player.gd
+++ game/src/Characters/Player/Player.gd
@@ -3,6 +3,7 @@ extends "res://src/Core/Grid/Gridable.gd"
 onready var Grid: = get_parent()
 
 func _ready() -> void:
+	# TODO: Ensure the player is aligned to "the grid" (aka a multiple of 32)
 	update_look_direction(Vector2.DOWN)
 
 
@@ -13,7 +14,12 @@ func _process(delta: float) -> void:
 
 	update_look_direction(input_direction)
 
-	var target_position: Vector2 = Grid.request_move(self, input_direction)
+	# This line tightly couples the player to the grid
+	#var target_position: Vector2 = Grid.request_move(self, input_direction)
+
+	# TEMP: Just move the player to the next "grid space" position
+	var target_position: Vector2 = position + (input_direction * 64)
+
 	if target_position:
 		move_to(target_position)
 	else:
@@ -21,6 +27,7 @@ func _process(delta: float) -> void:
 
 
 func get_input_direction() -> Vector2:
+	# TODO: experiment with disabling diagonal movement
 	return Vector2(
 		int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
 		int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
