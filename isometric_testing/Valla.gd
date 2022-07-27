6e3c3b0957df8dbc04b186306b62d2e7015887d5
Josh Maxwell <cctechwiz@gmail.com>
2022-07-27 05:42:05 -0600

    Adding test walking scene

---
+++ isometric_testing/Valla.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export(int) var speed = 200.0
+
+
+func _physics_process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1.0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1.0
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1.0
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1.0
+
+	velocity = velocity.normalized()
+
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("walk")
+		$AnimationTree.set("parameters/idle/blend_position", velocity)
+		$AnimationTree.set("parameters/walk/blend_position", velocity)
+		move_and_slide(velocity * speed)
